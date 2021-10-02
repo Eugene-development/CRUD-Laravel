a190bc9240ce7f2cc1bcf90b3affd1ea3dbb8aaf
indexpro24@gmail.com <indexpro24@gmail.com>
2021-10-02 17:19:37 +0300

    git init

--- src/app/Http/Services/Library/GetWhHmHmpmHmpmCo.php
+++ src/app/Http/Services/Library/GetWhHmHmpmHmpmCo.php
@@ -11,12 +11,15 @@ class GetWhHmHmpmHmpmCo
         return $this->model::where('project_id', $this->token)
             ->where('id', $param)
             ->with([
+                $this->thirdEntity  => function($query) {
+                    $query->where('project_id', $this->token);
+                }, //текст категории (полиморф)
                 $this->firstEntity => function($query) {
                     $query->where('project_id', $this->token);
                 },//все продукты проекта
                 $this->firstEntity . "." . $this->thirdEntity => function($query) {
                     $query->where('project_id', $this->token);
-                }, //текст категории (полиморф)
+                }, //текст продукта (полиморф)
                 $this->firstEntity . "." . $this->secondEntity => function($query) {
                     $query->where('project_id', $this->token);
                 },//изображение продукта (полиморф)
